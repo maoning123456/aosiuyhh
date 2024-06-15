b57aa1fb24c5f37608887bf8bfe385a530aa280e
kaisa <1724720397@qq.com>
2024-06-15 18:29:15 +0800

    初始化了项目

---
+++ src/main/java/com/qf/Hello.java
@@ -0,0 +1,15 @@
+package com.qf;
+
+/**
+ * @author Yi_Dan
+ * @version v1.0
+ * @project gitdemo01
+ * @package com.qf
+ * @company 千锋教育
+ * @date 2024/6/15 18:21
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
